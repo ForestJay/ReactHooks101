6d36f77c8688f2c02753c3e3c939fa610375b915
Forest Handford <forest.handford@homechef.com>
2023-11-22 14:59:31 -0500

    Got the last challenge page working
    TODO: Switch to using custom hooks!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const increase = () => setCount(count + 1)
-  const decrease = () => setCount(count - 1)
-  const reset = () => setCount(0)
+  const [person, setState] = useState({
+    firstName: '',
+    lastName: '',
+    age: 1
+  })
+
+  function handleFirstNameChange(event) {
+    setState({
+      ...person, firstName: event.target.value
+    })
+  }
+
+  function handleLastNameChange(event) {
+    setState({
+      ...person, lastName: event.target.value
+    })
+  }
+
+  function handleAgeChange(event) {
+    setState({
+      ...person, age: event.target.value
+    })
+  }
 
   return (
-    <div>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={reset}>Reset</button>
-      <h1>{count}</h1>
-    </div>
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={person.firstName}
+        onChange={handleFirstNameChange}
+      />
+
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={person.lastName}
+        onChange={handleLastNameChange}
+      />
+
+      <input
+        type="number"
+        placeholder="Age"
+        value={person.age}
+        onChange={handleAgeChange}
+      />
+    </form>
   )
 }
 
